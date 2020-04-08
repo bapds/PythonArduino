aecb00e4a7dedabb89066fe9a7c138ed952d31f2
bapds <bapds.1986@gmail.com>
2020-04-08 13:24:19 -0300

    Exemplo obtido na internet

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
 """
-Exemplo obtido na internet
-https://www.youtube.com/watch?v=vLuzO_vJrW4
-https://drive.google.com/file/d/1kFZamh1PqE2c6qmbjlA-HVlR-dtZjEDo/view
-acessado em 08/04/2020 as 11:05
+* BAPDS
+* Acionamento de barreira de Leds
+* Placa Arduino Mega 2560
 """
 from pyfirmata import Arduino, util
 import time
 
-                                #void setup(){
-Uno = Arduino('/dev/ttyACM0')
-                                     # Serial.begin(9600);
-print('Olá Mundo!')                  # Serial.println("Olá Mundo!");
-                                #}
+
+# função para ligar uma saída
+def ligar(placa, pino, delay):
+    placa.digital[pino].write(1)
+    print("LED ligado, pino: " + str(pino))
+    time.sleep(delay)  # delay(1000);
 
 
+# função para desligar uma saída
+def desligar(placa, pino, delay):
+    placa.digital[pino].write(0)
+    print("LED desligado, pino: " + str(pino))
+    time.sleep(delay)  # delay(1000);
 
-while True:                     # void loop(){
 
-    Uno.digital[13].write(1)         # digitalWrite(13, HIGH);
-    print('LED ligado')              # Serial.println("LED ligado");
-    time.sleep(0.5)                    # delay(1000);
+class BarLed():
 
-    Uno.digital[13].write(0)         # digitalWrite(13, LOW);
-    print('LED desligado')           # Serial.println("LED desligado");
-    time.sleep(1)                    # delay(1000);
+    def __init__(self, placa, pino, delay, funcao):
+        self.placa = placa
+        self.pino = pino
+        self.delay = delay
+        self.funcao = funcao
 
-                                #}
+        if self.funcao == 0:
+            ligar(self.placa, self.pino, self.delay)
+            desligar(self.placa, self.pino, self.delay)
+        elif self.funcao == 1:
+            ligar(self.placa, self.pino, self.delay)
+        elif self.funcao == 2:
+            desligar(self.placa, self.pino, self.delay)
+
+
+placa = Arduino('/dev/ttyACM1')
+pinos_btn = [2, 3, 4]
+pinos_leds = [5, 6, 7, 8, 9, 10, 11, 12, 13]
+tempo = 0.1 # delay
+
+print('Olá Mundo!')
+
+while True:
+
+    for pino in pinos_leds:
+        bar_led = BarLed(placa, pino, tempo, 0)
+    for pino in pinos_leds:
+        bar_led = BarLed(placa, pino, tempo, 1)
+    for pino in range(pinos_leds[-1], pinos_leds[0], -1):
+        bar_led = BarLed(placa, pino, tempo, 2)
